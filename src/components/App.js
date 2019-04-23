5c44743a212a2dbafd86d5ffd08404ecf8df2c0b
Enrique Pérez Arnaud <enrique@cazalla.net>
2019-04-23 13:57:06 +0200

    remove demo code, add app stub

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          SUNET Resources Inventory
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
