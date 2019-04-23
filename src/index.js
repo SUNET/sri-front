1a11a5c4d4bf0fe08040e408040b3d4c10e859db
Enrique Pérez Arnaud <enrique@cazalla.net>
2019-04-23 15:43:32 +0200

    missing import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, compose } from "redux";
+import { Provider } from 'react-redux';
 
 import appStore from "./store";
 import AppContainer from './containers/App';
