100f2808e6e1cf20996c6be33cfb1f9db7d477b6
Enrique Pérez Arnaud <enrique@cazalla.net>
2019-05-02 10:30:47 +0200

    wrong import

--- src/components/SRIButton.js
+++ src/components/SRIButton.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Button } from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
