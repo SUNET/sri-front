5ad88e5b8f5604932f3f4a270dcccf1c0759e2f1
Alvaro Navarro <anavarro@emergya.com>
2019-10-10 11:47:58 +0200

    remove get cookie on fetch

--- src/createRelayEnvironment.js
+++ src/createRelayEnvironment.js
@@ -1,30 +1,4 @@
-import {
-    Environment,
-    Network,
-    RecordSource,
-    Store,
-    QueryResponseCache
-} from "relay-runtime";
-// import $ from "jquery";
-
-// function getCookie(name) {
-//     var cookieValue = null;
-//     if (document.cookie && document.cookie !== "") {
-//         var cookies = document.cookie.split(";");
-//         for (var i = 0; i < cookies.length; i++) {
-//             var cookie = $.trim(cookies[i]);
-//             if (cookie.substring(0, name.length + 1) === name + "=") {
-//                 cookieValue = decodeURIComponent(
-//                     cookie.substring(name.length + 1)
-//                 );
-//                 break;
-//             }
-//         }
-//     }
-//     return cookieValue;
-// }
-//
-// var csrftoken = getCookie("csrftoken");
+import { Environment, Network, RecordSource, Store, QueryResponseCache } from "relay-runtime";
 
 // CACHING
 const oneMinute = 60 * 1000;
