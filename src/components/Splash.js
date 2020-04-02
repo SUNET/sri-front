18306df89c12c1a420a667ac6b9dcd01a184fa24
jlguillen <jlguillen@emergya.com>
2020-04-02 14:04:42 +0200

    add and configure eslint

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,18 +1,26 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-import "../style/Splash.scss";
+import '../style/Splash.scss';
 
 const Splash = ({ is_app_loaded }) => (
-    <>
-        {!is_app_loaded && (
-            <div id="splash-spinner">
-                <FontAwesomeIcon icon={faSpinner} className="fa-pulse fa-6x fa-fw" />
-            </div>
-        )}
-    </>
+  <>
+    {!is_app_loaded && (
+      <div id="splash-spinner">
+        <FontAwesomeIcon icon={faSpinner} className="fa-pulse fa-6x fa-fw" />
+      </div>
+    )}
+  </>
 );
+
+Splash.defaultProps = {
+  is_app_loaded: false,
+};
+
+Splash.propTypes = {
+  is_app_loaded: PropTypes.bool,
+};
 
 export default Splash;
