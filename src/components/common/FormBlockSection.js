55dd980e29f4b60f0da2b711bd8118e785464bfd
jlguillen <jlguillen@emergya.com>
2020-04-07 13:06:50 +0200

    add related component in common form

---
+++ src/components/common/FormBlockSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function FormBlockSection(props) {
+    const { editable, data } = props;
+    const isPresentState = !editable;
+    const presentContent = data.presentContent || "";
+    return (
+        <div className="form-internal-block__section">
+            <div className="form-internal-block__section__title">{data.title}</div>
+            <div
+                className={`form-internal-block__section__content ${
+                    editable ? "form-internal-block__section__content--edition-mode" : ""
+                }`}
+            >
+                {isPresentState ? presentContent : data.editContent}
+            </div>
+        </div>
+    );
+}
+
+export default FormBlockSection;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,18 @@
 // Forms IDs
-export const CREATE_CONTACT_FORM = 'create-contact-form';
-export const UPDATE_CONTACT_FORM = 'update-contact-form';
-export const CREATE_ORGANIZATION_FORM = 'create-organization-form';
-export const UPDATE_ORGANIZATION_FORM = 'update-organization-form';
-export const CREATE_GROUP_FORM = 'create-group-form';
-export const UPDATE_GROUP_FORM = 'update-group-form';
-export const CREATE_CUSTOMER_FORM = 'create-customer-form';
-export const UPDATE_CUSTOMER_FORM = 'update-customer-form';
-export const CREATE_PROVIDER_FORM = 'create-provider-form';
-export const UPDATE_PROVIDER_FORM = 'update-provider-form';
-export const CREATE_ENDUSER_FORM = 'create-end-user-form';
-export const UPDATE_ENDUSER_FORM = 'update-end-user-form';
-export const CREATE_SITEOWNER_FORM = 'create-site-owner-form';
-export const UPDATE_SITEOWNER_FORM = 'update-site-owner-form';
+export const CREATE_CONTACT_FORM = "create-contact-form";
+export const UPDATE_CONTACT_FORM = "update-contact-form";
+export const CREATE_ORGANIZATION_FORM = "create-organization-form";
+export const UPDATE_ORGANIZATION_FORM = "update-organization-form";
+export const CREATE_GROUP_FORM = "create-group-form";
+export const UPDATE_GROUP_FORM = "update-group-form";
+export const CREATE_CUSTOMER_FORM = "create-customer-form";
+export const UPDATE_CUSTOMER_FORM = "update-customer-form";
+export const CREATE_PROVIDER_FORM = "create-provider-form";
+export const UPDATE_PROVIDER_FORM = "update-provider-form";
+export const CREATE_ENDUSER_FORM = "create-end-user-form";
+export const UPDATE_ENDUSER_FORM = "update-end-user-form";
+export const CREATE_SITEOWNER_FORM = "create-site-owner-form";
+export const UPDATE_SITEOWNER_FORM = "update-site-owner-form";
 // Community Tabs
 export const COMMUNITY_ORGANIZATIONS = "organizations";
 export const COMMUNITY_GROUPS = "groups";
@@ -26,3 +26,12 @@ export const CONTACT_WORK = "work";
 
 // TIMES WAIT
 export const MILLISECONDS_TO_WAIT_REQUEST_AUTOCOMPLETE = 250;
+
+export const AFFILIATION_ORGANIZATION_NAMES = {
+    affiliation_customer: "Customer",
+    affiliation_end_customer: "End user",
+    affiliation_host_user: "Host user",
+    affiliation_partner: "Partner",
+    affiliation_provider: "Provider",
+    affiliation_site_owner: "Site owner",
+};
