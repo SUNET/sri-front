b0b888bb045b52592d8000eb69994d9fed444b3d
jlguillen <jlguillen@emergya.com>
2020-04-21 09:09:12 +0200

    add enzyme-to-json in order to build the snapshot

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
+import { createSerializer } from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
