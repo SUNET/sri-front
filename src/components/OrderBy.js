f9101f767778449d10b297d3d9a1fc48ef6ee426
jlguillen <jlguillen@emergya.com>
2020-04-22 13:34:31 +0200

    add order by test and refactor

--- src/components/OrderBy.js
+++ src/components/OrderBy.js
@@ -1,35 +1,42 @@
-import React from "react";
-import { withTranslation } from "react-i18next";
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
 
-import FieldSwitch from "./FieldSwitch";
+import FieldSwitch from './FieldSwitch';
 
-import "../style/OrderBy.scss";
+import '../style/OrderBy.scss';
 
-class OrderBy extends React.Component {
-    _handleOnChangeOrderBy = (event) => {
-        if (event.target.checked) {
-            this.props.changeOrderBy("handle_id_ASC");
-        } else {
-            this.props.changeOrderBy("handle_id_DESC");
-        }
-    };
-
-    render() {
-        const t = this.props.t;
-        return (
-            <div className="order-by outline">
-                <FieldSwitch
-                    type="toggle-icon"
-                    icon="angle"
-                    classNames="icon-right"
-                    labelChecked={t("filter.newest-first")}
-                    labelUnChecked={t("filter.latest-first")}
-                    handleChecked={(e) => this._handleOnChangeOrderBy(e)}
-                    id="orderBy"
-                />
-            </div>
-        );
+export class OrderBy extends React.Component {
+  handleOnChangeOrderBy(event) {
+    const { changeOrderBy } = this.props;
+    if (event.target.checked) {
+      changeOrderBy('handle_id_ASC');
+    } else {
+      changeOrderBy('handle_id_DESC');
     }
+  }
+
+  render() {
+    const { t } = this.props;
+    return (
+      <div className="order-by outline">
+        <FieldSwitch
+          type="toggle-icon"
+          icon="angle"
+          classNames="icon-right"
+          labelChecked={t('filter.newest-first')}
+          labelUnChecked={t('filter.latest-first')}
+          handleChecked={(e) => this.handleOnChangeOrderBy(e)}
+          id="orderBy"
+        />
+      </div>
+    );
+  }
 }
+
+OrderBy.propTypes = {
+  changeOrderBy: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+};
 
 export default withTranslation()(OrderBy);
