1c98c978c8790b2ca71056395bc720b359344017
jlguillen <jlguillen@emergya.com>
2020-04-27 09:07:14 +0200

    add rangedaypicker test

--- src/__tests__/common/RangeDayPicker.test.js
+++ src/__tests__/common/RangeDayPicker.test.js
@@ -8,7 +8,6 @@ let RangeDayPickerComponent;
 let RangeDayPickerComponentInstance;
 
 const defaultProps = {
-  changeRangeDayPicker: jest.fn(),
   dateTo: jest.fn(),
   dateFrom: jest.fn(),
   resetDate: jest.fn(),
@@ -38,7 +37,7 @@ beforeEach(() => {
   RangeDayPickerWrapper = mountRangeDayPickerWrapper();
   RangeDayPickerComponent = RangeDayPickerWrapper.find('RangeDayPicker');
   RangeDayPickerComponentInstance = RangeDayPickerComponent.instance();
-  RangeDayPickerComponentInstance.changeRangeDayPickerDebounce = RangeDayPickerComponentInstance.changeRangeDayPicker;
+  RangeDayPickerComponentInstance.showFromMonth = jest.fn();
 });
 afterEach(() => {
   RangeDayPickerWrapper.unmount();
@@ -74,5 +73,47 @@ describe('Elements', () => {
 });
 
 describe('Add Events', () => {
-  it('', () => {});
+  it('Add a date in from input', () => {
+    const pickerElements = getDatePickerElements('.input-from-to--from');
+    const input = pickerElements.rangeDayPicker.find('input');
+    input.simulate('change', { target: { value: '30/05/20' } });
+    expect(RangeDayPickerComponent.state().from.toString()).toBe('Sat May 30 2020 00:00:00 GMT+0200 (GMT+02:00)');
+    expect(defaultProps.dateFrom.mock.calls[0][0].toString()).toBe('Sat May 30 2020 00:00:00 GMT+0200 (GMT+02:00)');
+  });
+  it('Add a date in to input ', () => {
+    const pickerElementsTo = getDatePickerElements('.input-from-to--to');
+    const inputTo = pickerElementsTo.rangeDayPicker.find('input');
+    inputTo.simulate('change', { target: { value: '30/05/20' } });
+    expect(defaultProps.dateTo.mock.calls[0][0].toString()).toBe('Sat May 30 2020 00:00:00 GMT+0200 (GMT+02:00)');
+  });
+  it('Filled from and to inputs', () => {
+    const pickerElementsTo = getDatePickerElements('.input-from-to--to');
+    const pickerElements = getDatePickerElements('.input-from-to--from');
+    const inputTo = pickerElementsTo.rangeDayPicker.find('input');
+    const inputFrom = pickerElements.rangeDayPicker.find('input');
+    inputFrom.simulate('change', { target: { value: '29/05/20' } });
+    inputTo.simulate('change', { target: { value: '30/05/20' } });
+    expect(defaultProps.dateFrom.mock.calls[0][0].toString()).toBe('Fri May 29 2020 00:00:00 GMT+0200 (GMT+02:00)');
+    expect(defaultProps.dateTo.mock.calls[0][0].toString()).toBe('Sat May 30 2020 00:00:00 GMT+0200 (GMT+02:00)');
+    expect(RangeDayPickerComponentInstance.showFromMonth.mock.calls).toHaveLength(1);
+  });
+
+  it('Click in reset dates ', () => {
+    RangeDayPickerComponentInstance.setState = jest.fn();
+    RangeDayPickerComponentInstance.toRef.current.setState = jest.fn();
+    RangeDayPickerComponentInstance.fromRef.current.setState = jest.fn();
+    const resetButton = RangeDayPickerComponent.find('[data-action="reset"]');
+    resetButton.simulate('click');
+    expect(defaultProps.resetDate.mock.calls[0][0]).toBeUndefined();
+    expect(defaultProps.resetDate.mock.calls[0][1]).toBeUndefined();
+    expect(RangeDayPickerComponentInstance.setState.mock.calls[0][0]).toEqual({ from: undefined, to: undefined });
+    expect(RangeDayPickerComponentInstance.fromRef.current.setState.mock.calls[0][0]).toEqual({
+      value: '',
+      typedValue: '',
+    });
+    expect(RangeDayPickerComponentInstance.toRef.current.setState.mock.calls[0][0]).toEqual({
+      value: '',
+      typedValue: '',
+    });
+  });
 });
--- src/components/RangeDayPicker.js
+++ src/components/RangeDayPicker.js
@@ -1,119 +1,128 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
-import moment from "moment";
-import DayPickerInput from "react-day-picker/DayPickerInput";
-import MomentLocaleUtils, { formatDate, parseDate } from "react-day-picker/moment";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
+import moment from 'moment';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import MomentLocaleUtils, { formatDate, parseDate } from 'react-day-picker/moment';
 
-import "moment/locale/en-gb";
-import "moment/locale/es";
-import "moment/locale/sv";
+import 'moment/locale/en-gb';
+import 'moment/locale/es';
+import 'moment/locale/sv';
 
-import "react-day-picker/lib/style.css";
-import "../style/RangeDayPicker.scss";
+import 'react-day-picker/lib/style.css';
+import '../style/RangeDayPicker.scss';
 
 export class RangeDayPicker extends React.Component {
-    // static propTypes = {
-    //     resetDate: PropTypes.func.isRequired
-    // };
-
-    constructor(props) {
-        super(props);
-        this.handleResetClick = this.handleResetClick.bind(this);
-        this.from = null; // elements reference
-        this.to = null; // elements reference
-        this.state = {
-            from: undefined,
-            to: undefined
-        };
-    }
-
-    showFromMonth() {
-        const { from, to } = this.state;
-        if (!from) {
-            return;
-        }
-        if (moment(to).diff(moment(from), "months") < 2) {
-            console.log('from: ', from);
-            this.to.getDayPicker().showMonth(from);
-        }
-    }
-
-    handleFromChange = (from, modifiers, dayPickerInput) => {
-        // Change the from date and focus the "to" input field
-        this.setState({ from }, this.props.dateFrom(from));
+  constructor(props) {
+    super(props);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.fromRef = React.createRef();
+    this.toRef = React.createRef();
+    this.state = {
+      from: undefined,
+      to: undefined,
     };
+  }
 
-    handleToChange = (to, modifiers, dayPickerInput) => {
-        this.setState({ to }, this.showFromMonth, this.props.dateTo(to));
-    };
+  showFromMonth() {
+    const { from, to } = this.state;
+    if (!from) {
+      return;
+    }
+    if (moment(to).diff(moment(from), 'months') < 2) {
+      console.log('from: ', from);
+      console.log('this.to.getDayPicker(): ', this.toRef.current.getDayPicker().showMonth);
+      this.toRef.current.getDayPicker().showMonth(from);
+    }
+  }
 
-    handleResetClick = () => {
-        this.setState({ from: undefined, to: undefined }, this.props.resetDate(undefined, undefined));
-        // this fixed the encapsulated input uncontrolled bug
-        this.from.setState({ value: "", typedValue: "" });
-        this.to.setState({ value: "", typedValue: "" });
-    };
+  handleFromChange = (from, modifiers, dayPickerInput) => {
+    // Change the from date and focus the "to" input field
+    this.setState({ from }, this.props.dateFrom(from));
+  };
 
-    renderDayPicker(fromOrToInput) {
-        const { from, to } = this.state;
-        const { t } = this.props;
-        const modifiers = { start: from, end: to };
-        const isFrom = fromOrToInput === "from";
-        let datePickerConfig = {};
-        if (isFrom) {
-            datePickerConfig = {
-                disabledDays: { after: to },
-                toMonth: to,
-                onDayClick: () => this.to.getInput().focus()
-            };
-        } else {
-            datePickerConfig = {
-                disabledDays: { before: from },
-                month: from,
-                fromMonth: from
-            };
-        }
-        return (
-            <div className={`input-from-to input-from-to--${fromOrToInput}`}>
-                <label>{t(`filter.daypicker.${fromOrToInput}`)}</label>
-                <DayPickerInput
-                    value={this.state[fromOrToInput]}
-                    ref={(el) => (this[fromOrToInput] = el)}
-                    placeholder="dd/mm/yy"
-                    format="DD/MM/YY"
-                    formatDate={formatDate}
-                    parseDate={parseDate}
-                    dayPickerProps={{
-                        ...datePickerConfig,
-                        locale: "en",
-                        localeUtils: MomentLocaleUtils,
-                        selectedDays: [from, { from, to }],
-                        modifiers,
-                        numberOfMonths: 2
-                    }}
-                    onDayChange={isFrom ? this.handleFromChange : this.handleToChange}
-                />
-            </div>
-        );
+  handleToChange = (to, modifiers, dayPickerInput) => {
+    this.setState({ to }, this.showFromMonth, this.props.dateTo(to));
+  };
+
+  handleResetClick = () => {
+    this.setState({ from: undefined, to: undefined }, this.props.resetDate(undefined, undefined));
+    // this fixed the encapsulated input uncontrolled bug
+    this.fromRef.current.setState({ value: '', typedValue: '' });
+    this.toRef.current.setState({ value: '', typedValue: '' });
+  };
+
+  renderDayPicker(fromOrToInput) {
+    const { from, to } = this.state;
+    const { t } = this.props;
+    const modifiers = { start: from, end: to };
+    const isFrom = fromOrToInput === 'from';
+    let datePickerConfig = {};
+    if (isFrom) {
+      datePickerConfig = {
+        disabledDays: { after: to },
+        toMonth: to,
+        onDayClick: () => this.toRef.current.getInput().focus(),
+      };
+    } else {
+      datePickerConfig = {
+        disabledDays: { before: from },
+        month: from,
+        fromMonth: from,
+      };
     }
-    renderResetDatesCTA() {
-        const { t } = this.props;
-        return (
-            <button type="button" onClick={this.handleResetClick} data-action="reset" className="range-day-picker__reset_dates btn outline btn-remove">
-                {t("filter.date.clear")}
-            </button>
-        );
-    }
-    render() {
-        return (
-            <div className="range-day-picker">
-                {this.renderDayPicker("from")}
-                {this.renderDayPicker("to")}
-                {this.renderResetDatesCTA()}
-            </div>
-        );
-    }
+    return (
+      <div className={`input-from-to input-from-to--${fromOrToInput}`}>
+        <label>{t(`filter.daypicker.${fromOrToInput}`)}</label>
+        <DayPickerInput
+          value={this.state[fromOrToInput]}
+          ref={this[`${fromOrToInput}Ref`]}
+          placeholder="dd/mm/yy"
+          format="DD/MM/YY"
+          formatDate={formatDate}
+          parseDate={parseDate}
+          dayPickerProps={{
+            ...datePickerConfig,
+            locale: 'en',
+            localeUtils: MomentLocaleUtils,
+            selectedDays: [from, { from, to }],
+            modifiers,
+            numberOfMonths: 2,
+          }}
+          onDayChange={isFrom ? this.handleFromChange : this.handleToChange}
+        />
+      </div>
+    );
+  }
+  renderResetDatesCTA() {
+    const { t } = this.props;
+    return (
+      <button
+        type="button"
+        onClick={this.handleResetClick}
+        data-action="reset"
+        className="range-day-picker__reset_dates btn outline btn-remove"
+      >
+        {t('filter.date.clear')}
+      </button>
+    );
+  }
+  render() {
+    return (
+      <div className="range-day-picker">
+        {this.renderDayPicker('from')}
+        {this.renderDayPicker('to')}
+        {this.renderResetDatesCTA()}
+      </div>
+    );
+  }
 }
+
+RangeDayPicker.propTypes = {
+  dateTo: PropTypes.func.isRequired,
+  dateFrom: PropTypes.func.isRequired,
+  resetDate: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+};
 
 export default withTranslation()(RangeDayPicker);
