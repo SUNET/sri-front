e7edc15b8c8e39715df11303a0b77fd06dcf341b
jlguillen <jlguillen@emergya.com>
2020-06-03 13:19:22 +0200

    merge and fix duplicate function

--- src/sagas/common.js
+++ src/sagas/common.js
@@ -1,63 +1,63 @@
 import CONFIG from '../config';
+
 const { API_HOST } = CONFIG;
 
-export let _csrfToken = null;
+export async function getCsrfToken() {
+  let _csrfToken = null;
+  if (_csrfToken === null) {
+    const response = await fetch(`${API_HOST}/csrf/`, {
+      credentials: 'include',
+    });
+    const data = await response.json();
+    _csrfToken = data.csrfToken;
+  }
 
-async function getCsrfToken() {
-    if (_csrfToken === null) {
-        const response = await fetch(`${API_HOST}/csrf/`, {
-            credentials: "include"
-        });
-        const data = await response.json();
-        _csrfToken = data.csrfToken;
-    }
-
-    return _csrfToken;
+  return _csrfToken;
 }
 
 async function getHeaders() {
-    return  {
-        "Content-Type": "application/json; charset=utf-8",
-        Accept: "application/json",
-        "Accept-Encoding": "gzip,deflate",
-        "X-Requested-With": "XMLHttpRequest",
-        "X-CSRFToken": await getCsrfToken()
-    }
+  return {
+    'Content-Type': 'application/json; charset=utf-8',
+    Accept: 'application/json',
+    'Accept-Encoding': 'gzip,deflate',
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRFToken': await getCsrfToken(),
+  };
 }
 
 export const ajaxHeaders = {
-    "Content-Type": "application/json; charset=utf-8",
-    Accept: "application/json",
-    "Accept-Encoding": "gzip,deflate",
-    "X-Requested-With": "XMLHttpRequest",
-    "X-CSRFToken": getCsrfToken()
+  'Content-Type': 'application/json; charset=utf-8',
+  Accept: 'application/json',
+  'Accept-Encoding': 'gzip,deflate',
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRFToken': getCsrfToken(),
 };
 
 export const putRequest = {
-    method: "put",
-    redirect: "manual",
-    credentials: "include",
-    headers: getHeaders()
+  method: 'put',
+  redirect: 'manual',
+  credentials: 'include',
+  headers: getHeaders(),
 };
 
 export const postRequest = {
-    method: "post",
-    redirect: "manual",
-    credentials: "include",
-    headers: getHeaders()
+  method: 'post',
+  redirect: 'manual',
+  credentials: 'include',
+  headers: getHeaders(),
 };
 
 export const getRequest = {
-    method: "get",
-    redirect: "manual",
-    credentials: "include",
-    headers: getHeaders()
+  method: 'get',
+  redirect: 'manual',
+  credentials: 'include',
+  headers: getHeaders(),
 };
 
 export const checkStatus = function(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return response;
-    } else {
-        throw new Error(response.statusText);
-    }
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(response.statusText);
+  }
 };
