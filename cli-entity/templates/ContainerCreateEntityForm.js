042c5d59ee7858ee14b52aa0a1b6b63a8076d1df
jlguillen <jlguillen@emergya.com>
2020-06-22 07:21:33 +0200

    js style in template files

--- cli-entity/templates/ContainerCreateEntityForm.js
+++ cli-entity/templates/ContainerCreateEntityForm.js
@@ -1,31 +1,31 @@
-import { connect } from "react-redux";
-import { getFormMeta, getFormSyncErrors, formValueSelector } from "redux-form";
+import { connect } from 'react-redux';
+import { getFormMeta, getFormSyncErrors, formValueSelector } from 'redux-form';
 
-import * as actions from "../../actions/Notify";
-import Create__EntityClassName__Form from "../../components/__entityName__/Create__EntityClassName__Form";
+import * as actions from '../../actions/Notify';
+import Create__EntityClassName__Form from '../../components/__entityName__/Create__EntityClassName__Form';
 
 const mapStateToProps = (state, props) => {
-    const update__EntityClassName__Selector = formValueSelector("create__EntityClassName__");
-    return {
-        fields: getFormMeta("create__EntityClassName__")(state),
-        formSyncErrors: getFormSyncErrors("create__EntityClassName__")(state),
-        name: update__EntityClassName__Selector(state, "name"),
-        url: update__EntityClassName__Selector(state, "url")
-    };
+  const update__EntityClassName__Selector = formValueSelector('create__EntityClassName__');
+  return {
+    fields: getFormMeta('create__EntityClassName__')(state),
+    formSyncErrors: getFormSyncErrors('create__EntityClassName__')(state),
+    name: update__EntityClassName__Selector(state, 'name'),
+    url: update__EntityClassName__Selector(state, 'url'),
+  };
 };
 
 const mapDispatchToProps = (dispatch, props) => {
-    return {
-        notify: (msg, level) => {
-            dispatch(actions.notify(msg, level));
-        },
-        showNewContactForm
-    };
+  return {
+    notify: (msg, level) => {
+      dispatch(actions.notify(msg, level));
+    },
+    showNewContactForm,
+  };
 };
 
 const Create__EntityClassName__FormContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps,
 )(Create__EntityClassName__Form);
 
 export default Create__EntityClassName__FormContainer;
--- cli-entity/templates/QueryEntityDetailsQuery.js
+++ cli-entity/templates/QueryEntityDetailsQuery.js
@@ -1,38 +1,38 @@
-import graphql from "babel-plugin-relay/macro";
+import graphql from 'babel-plugin-relay/macro';
 
 const __EntityClassName__DetailsQuery = graphql`
-    query __EntityClassName__DetailsQuery($__entityName__Id: ID!) {
-        get__EntityClassName__ById(id: $__entityName__Id) {
-            ...__EntityClassName__UpdateForm___entityName__
-            id
-            name
-            description
-            url
-            __typename
-            with_same_name {
-                id
-                name
-                __typename
-            }
-            comments {
-                id
-                user {
-                    first_name
-                    last_name
-                }
-                comment
-                submit_date
-            }
-            created
-            creator {
-                email
-            }
-            modified
-            modifier {
-                email
-            }
+  query __EntityClassName__DetailsQuery($__entityName__Id: ID!) {
+    get__EntityClassName__ById(id: $__entityName__Id) {
+      ...__EntityClassName__UpdateForm___entityName__
+      id
+      name
+      description
+      url
+      __typename
+      with_same_name {
+        id
+        name
+        __typename
+      }
+      comments {
+        id
+        user {
+          first_name
+          last_name
         }
+        comment
+        submit_date
+      }
+      created
+      creator {
+        email
+      }
+      modified
+      modifier {
+        email
+      }
     }
+  }
 `;
 
 export default __EntityClassName__DetailsQuery;
