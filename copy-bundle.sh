33b38fc6ce5d263e14fde12ca0b8218911d9a9a5
Francisco Fuentes <ffuentes@emergya.com>
2020-09-09 12:21:11 +0200

    Compressed find lines

--- copy-bundle.sh
+++ copy-bundle.sh
@@ -5,8 +5,8 @@ set -x
 
 rm -rf /bundle/*
 
-find build/. -type f -name "*.js" -print0 | xargs -0 sed -i -e 's@__API_HOST__@'"$REACT_APP_API_HOST"'@g'
-find build/. -type f -name "*.js" -print0 | xargs -0 sed -i -e 's@__COOKIE_DOMAIN__@'"$REACT_APP_COOKIE_DOMAIN"'@g'
-find build/. -type f -name "*.js" -print0 | xargs -0 sed -i -e 's@__SCHEMA_VERSION__@'"$SCHEMA_VERSION"'@g'
+find build/. -type f -name "*.js" -exec sed -i 's@__API_HOST__@'"$REACT_APP_API_HOST"'@g' {} \;
+find build/. -type f -name "*.js" -exec sed -i 's@__COOKIE_DOMAIN__@'"$REACT_APP_COOKIE_DOMAIN"'@g' {} \;
+find build/. -type f -name "*.js" -exec sed -i 's@__SCHEMA_VERSION__@'"$SCHEMA_VERSION"'@g' {} \;
 
 cp -r build/* /bundle/
