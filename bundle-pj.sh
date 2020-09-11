842b0f8a73bc568e17fe2f26e5d8a07623240b57
Francisco Fuentes <ffuentes@emergya.com>
2020-09-11 12:36:39 +0200

    Change from node to node-alpine

--- bundle-pj.sh
+++ bundle-pj.sh
@@ -3,9 +3,14 @@
 set -e
 set -x
 
+rm -rf /app/node_modules
+
 npm i
 npm run adapt-queries common
 npm run relay-common
 npm run build
+
+cp -r /app/build/* /bundle/
+rm -rf /app/build/*
 
 sh ./copy-bundle.sh
