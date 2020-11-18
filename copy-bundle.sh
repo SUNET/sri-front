9c7e054626fe528fdf45ad312ff4bc183547ccc7
oilegor1029 <rogelioramos96@gmail.com>
2020-11-18 15:26:02 +0100

    Handle non-existing /bundle dir

--- copy-bundle.sh
+++ copy-bundle.sh
@@ -3,8 +3,9 @@
 set -e
 set -x
 
-rm -r /bundle || true
-cp -r /app/build /bundle
+mkdir -p /bundle
+rm -r /bundle/* || true
+cp -r /app/build/* /bundle/
 
 find /bundle/. -type f -name "*.js" -print0 | xargs -0 sed -i -e 's@__API_HOST__@'"$REACT_APP_API_HOST"'@g'
 find /bundle/. -type f -name "*.js" -print0 | xargs -0 sed -i -e 's@__COOKIE_DOMAIN__@'"$REACT_APP_COOKIE_DOMAIN"'@g'
