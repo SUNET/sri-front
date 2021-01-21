26f1b03da992e5d7dcef25ab8c85d54906350fb0
Francisco Fuentes <ffuentes@emergya.com>
2021-01-21 14:47:52 +0100

    Added FORMS_VERSION envvar to config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ const API_HOST_SUFFIX = '__API_HOST__';
 const COOKIE_DOMAIN = '__COOKIE_DOMAIN__';
 const SCHEMA_VERSION = '__SCHEMA_VERSION__';
 const LOGIN_URL = '__LOGIN_URL__';
+const FORMS_VERSION = '__FORMS_VERSION__';
+
 
 export default {
   API_HOST: `${PROTOCOL}//${API_HOST_SUFFIX}`,
