11c27e02740306b9a17e3f845278849d747e5d25
Francisco Fuentes <ffuentes@emergya.com>
2021-01-25 15:00:08 +0100

    parametrized output schema file

--- bundle-pj.sh
+++ bundle-pj.sh
@@ -10,7 +10,7 @@ export SCHEMA_VERSION=$(curl http://sri-nginx/api/forms/)
 
 # get schema
 npm install get-graphql-schema
-npx get-graphql-schema http://sri-nginx/api/graphql/ > schema_common.graphql
+npx get-graphql-schema http://sri-nginx/api/graphql/ > schema_${SCHEMA_VERSION}.graphql
 
 # make bundle
 npm i
